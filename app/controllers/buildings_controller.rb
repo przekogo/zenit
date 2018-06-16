73a6ec9b104c04f45b1ce1356477dc0bc709bb85
Unknown <przeroog@gmail.com>
2018-06-17 01:32:12 +0200

    buildings tab for lg-up
    gates component, main color variables

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,3 @@
+class BuildingsController < ApplicationController
+  def index ; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+  get 'buildings', to: 'buildings#index'
   get 'contact', to: 'contact#index'
 
   post 'contact/mail', to: 'contact#mail', format: 'js'
