19c2bead4e409d27e01ab5d14611cd10d386dbc6
przekogo <przeroog@gmail.com>
2018-08-30 21:04:29 +0200

    collapsible news + some styling
    remove unused code

--- app/assets/javascripts/news.js
+++ app/assets/javascripts/news.js
@@ -6,13 +6,38 @@ function initNews() {
   $container = $(this);
   $wrapper = $container.find('.rz-news__wrapper');
   $scrollbar = $container.find('.scrollbar');
+  $articles = $wrapper.find('.rz-news__article');
+
   $container.on('scroll', function() {
     console.log(scrollPercentage());
     $scrollbar.css("background-image", "linear-gradient(rgba(179, 102, 255, 0), rgba(179, 102, 255, 1) " + scrollPercentage() + "%, rgba(179, 102, 255, 0))");
   });
 
+  $articles.on('click', function() {
+    $(this).toggleClass('active');
+    toggleCollapse($(this).find('.content'));
+  });
+
   function scrollPercentage() {
     // TODO: add a timeout here and maybe css transition to improve performace
     return 100 * $container.scrollTop() / ($wrapper.outerHeight() - $container.outerHeight())
+  }
+
+  function toggleCollapse($collapsible) {
+    if ($collapsible.css('max-height') == '0px') {
+      expandArticle($collapsible);
+    }
+    else {
+      collapseArticle($collapsible);
+    }
+  }
+
+  function expandArticle($collapsible) {
+      console.log($collapsible.children('p').outerHeight());
+    $collapsible.css('max-height', $collapsible.find('p').outerHeight());
+  }
+
+  function collapseArticle($collapsible) {
+    $collapsible.css('max-height', '0px');
   }
 }
