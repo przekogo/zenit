0378fe66a1048c4cc21433ad1db639fb54eaa06e
Przemek R <przeroog@gmail.com>
2019-02-01 16:46:44 +0100

    orchid gallery assets

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -9,7 +9,7 @@ class GalleriesController < ApplicationController
           images: {
             lotus: ['lotus-1.jpg'],
             lily: ['lily-1.jpg'],
-            orchid: ['orchid-1.jpg', 'orchid-2.jpg', 'orchid-3.jpg'],
+            orchid: ['orchid-1.jpg', 'orchid-2.jpg', 'orchid-3.jpg', 'orchid-4.jpg', 'orchid-5.jpg', 'orchid-6.jpg', 'orchid-7.jpg'],
             rose: ['rose-1.jpg', 'rose-2.jpg', 'rose-3.jpg', 'rose-4.jpg'],
             iris: ['iris-1.jpg']
           }[params[:building].to_sym]
