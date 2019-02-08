c02589cffdfc3662b5c0d632a21365cdbdc82ed9
Przemek R <przeroog@gmail.com>
2019-02-08 14:37:47 +0100

    Revert "enable cors"
    This reverts commit 4628a9a294dffb2d5effd80d05fe25264bba5bb5.

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ gem 'haml'
 gem 'activeadmin'
 gem 'devise'
 gem 'roman-numerals'
-gem 'rack-cors', :require => 'rack/cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Zenit
   class Application < Rails::Application
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
-      end
-    end
 
     ActionMailer::Base.smtp_settings = {
       address:        Rails.application.secrets.sendgrid[:address],
